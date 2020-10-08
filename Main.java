e6dbffd43711ab5417fd36a24f4dbfb021894576
balez4464 <balez4464@wrdsb.ca>
2020-10-08 15:18:28 +0000

    made a program that allows user to find the largest number out of their ten

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *This program allows the user to enter numbers and finds the largest number within the array
+ * @author Zachary Balean
  */
 public class Main {
 
@@ -9,7 +10,38 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // creates Scanner
+    Scanner input = new Scanner(System.in);
+
+    //asks user to put in numbers
+    System.out.println("Please enter in 10 integers to put into the array");
+
+    //creates a constant for the array
+    final int NUM_AMOUNT = 10;
+
+    //creates array for the nummebrs
+    int numbers[] = new int[NUM_AMOUNT];
+
+    //for loop so user can write down their numbers
+    for (int i = 0; i < numbers.length; i++){
+      //allows them to print in thier numbers
+      numbers[i] = input.nextInt();
+    }
+
+    //variable to find the largest value
+    int comparer = numbers[0];
+
+    //for loop to compare the greatest value to
+    for (int i = 0; i < numbers.length; i++){
+      //if the comparer is less then the given number
+      if (comparer < numbers[i]){
+        //makes the comaprer the largest out of the group
+        comparer = numbers[i];
+      }
+    }
+    //prints out largest number
+    System.out.println("The largest number is " + comparer);
+
+
   }
 }
